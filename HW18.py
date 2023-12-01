1eada04f8acd271a329e74d490540b1a73aa7e64
Yemelianova Viktoriia <viktoriia.eme96@gmail.com>
2023-12-01 12:31:44 +0200

    HW

--- HW18.py
+++ HW18.py
@@ -1,11 +1,14 @@
 def common_elements():
-    set_1 = {3, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63}
-    set_2 = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70}
+    set_1 = set(range(3, 100, 3))
+    set_2 = set(range(5, 100, 5))
+
 
     common = set_1.intersection(set_2)
     return common
 
 print(common_elements())
+
+
 
 
 
